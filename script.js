5ac6236258a404ff622e0d23f29dd7adfc3e374b
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-25 10:28:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //your JS code here. If required.
-document.getElementByClassNames("form-group").addEventListener("submit",function(e){
+document.getElementsByClassName("form-group").addEventListener("submit",function(e){
 e.preventDefault();
 	const title=document.getElementById("title").value;
 		const author=document.getElementById("author").value;
 		const isbn=document.getElementById("isbn").value;
 
 		const table=document.getElementById("book-list").
-			getElementByTagName("tbody")[0];
+			getElementsByTagName("tbody")[0];
 	const newrow=table.insertRow(table.rows.length);
 
-	const cell1=newrow.incertCell(0);
-	const cell2=newrow.incertCell(1);
-	const cell3=newrow.incertCell(2);
-	const cell4=newrow.incertCell(3);
+	const cell1=newrow.insertCell(0);
+	const cell2=newrow.insertCell(1);
+	const cell3=newrow.insertCell(2);
+	const cell4=newrow.insertCell(3);
 
-	cell1.innerHtml=title;
-	cell2.innerHtml=author;
-	cell3.innerHtml=isbn;
-	cell4.innerHtml=`<button class="delete">X</button>`;
+	cell1.innerHTML=title;
+	cell2.innerHTML=author;
+	cell3.innerHTML=isbn;
+	cell4.innerHTML=`<button class="delete">X</button>`;
 
 	document.getElementById("title").value="";
 	document.getElementById("author").value="";
