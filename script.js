a50d41e81c4fc04ef990f02fe29c19e181f5d1d5
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-26 12:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 //your JS code here. If required.
-document.querySelector(".form-group").addEventListener("submit",function(e){
-e.preventDefault();
+function addbook() {
+	
+
 	const title=document.getElementById("title").value;
 		const author=document.getElementById("author").value;
 		const isbn=document.getElementById("isbn").value;
 
+	if(title ==="" || author ==="" || isbn==="")
+	{
+		alert("Please fill in all fields");
+		return;
+	}
+
 		const table=document.getElementById("book-list").
-			getElementsByTagName("tbody")[0];
-	const newrow=table.insertRow(table)
+			
+	const newrow = document.createElement("tr");
 
-	const cell1=newrow.insertCell(0);
-	const cell2=newrow.insertCell(1);
-	const cell3=newrow.insertCell(2);
+	newrow.innerHTML=`
+	<td>${title}</td>
+	<td>${author}</td>
+	<td>${isbn}</td>
+	<td><button class="delete" onclick="removebook(this)">X</button>
+	`;
+	table.appendChild(newrow);
 	
-	cell1.innerHTML=title;
-	cell2.innerHTML=author;
-	cell3.innerHTML=isbn;
-
-	const cell4=newrow.insertCell(3);
-	cell4.innerHTML=`<button class="delete">X</button>`;
-
 	document.getElementById("title").value="";
 	document.getElementById("author").value="";
 	document.getElementById("isbn").value="";
 		
-});
-
-document.getElementById("book-list").addEventListener("click",function(e){
-if(e.target.classList.contains("delete"))	
-{
-	e.target.parentElement.parentElement.remove();
 }
-});
+function removebook(btn) {
+	btn.parentElement.parentElement.remove();
+}
 
 
